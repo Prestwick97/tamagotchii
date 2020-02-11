95b4e387543320445f7394e39fc375c8d7829c61
Student <example@example.com>
2020-02-10 16:55:13 -0800

    refactor code to use switch statement and update tests.

--- __tests__/tamagotchi.test.js
+++ __tests__/tamagotchi.test.js
@@ -8,37 +8,31 @@ import { Cat } from './../src/cat.js';
 // import { toExpression, tsImportEqualsDeclaration } from '@babel/types';
 
 
-describe("Pet", ()=> {
-
-    
-});
 
 describe("Sparky", () => {
     jest.useFakeTimers();
     let sparky;
     beforeEach(function() {
         sparky = new Pet("Sparky");
-        sparky.newDog();
-        sparky.namePet("Sparky");
     });
     afterEach(function() {
         jest.clearAllTimers();
     });
 
     test("creates dog object with name and properties at full value", () => {
-        sparky.namePet("Sparky");
         expect(sparky.bath).toEqual(10);
-        console.log(sparky);
+        // console.log(sparky);
         
     });
     test("food attribute reduces by 1 every 5 seconds", () => {
         sparky.foodDecDog();
-        jest.advanceTimersByTime(10001);
+        jest.advanceTimersByTime(10000);
         expect(sparky.food).toEqual(8);
+        console.log(sparky);
     });
     test("food level increaeses back to 10 when user 'feeds' dog", () => {
        sparky.foodDecDog();
-       jest.advanceTimersByTime(10001);
+       jest.advanceTimersByTime(1000);
        sparky.feed();
        expect(sparky.food).toEqual(10);
     });
@@ -48,6 +42,15 @@ describe("Sparky", () => {
         jest.advanceTimersByTime(50001);
         console.log(sparky);
         expect(sparky.type).toEqual("cat");
+    });
+    test("if food property reduces to zero for type cat, cat turns into a fishwith properties at full value", () => {
+        sparky.food = 0;
+        sparky.foodDecDog();
+        jest.advanceTimersByTime(50001);
+        sparky.food = 0;
+        jest.advanceTimersByTime(70001);
+        expect(sparky.type).toEqual("fish");
+        // console.log(sparky); // now a fish
     });
 
 });
--- src/pet.js
+++ src/pet.js
@@ -1,82 +1,87 @@
 export class Pet {
-    constructor(name, food, play, bath, discipline, potty, sleep, snuggles){
+    constructor(name){
         this.name = name;
-        this.food = food;
-        this.play = play;
-        this.bath = bath;
-        this.discipline = discipline;
-        this.potty = potty;
-        this.sleep = sleep;
-        this.snuggles = snuggles;
-    }   
-
-namePet(name) {
-    this.name = name;
-}
-
-
-feed() {
-    this.food = 10;
-}
-
-playWith() {
-    this.play = 10;
-}
-
-bathe() {
-    this.bath = 10;
-}
-
-train() {
-    this.discipline = 10;
-}
-
-waste() {
-    this.potty = 10;
-}
-
-rest() {
-    this.sleep = 10;
-}
-
-love() {
-    this.snuggles = 10;
-}
-
-newDog() {
-    this.name = name;
-    this.food = 10;
-    this.play = 10;
-    this.bath = 10;
-    this.discipline = 10;
-    this.potty = 10;
-    this.sleep = 10;
-    this.snuggles = 10;
-    this.type = "dog";
-}
-
-foodDecDog() {
-    setInterval(() => {
-        this.food--;
-    }, 5000);
-    if(this.food <= 0) {
         this.food = 10;
         this.play = 10;
+        this.bath = 10;
+        this.discipline = 10;
         this.potty = 10;
+        this.sleep = 10;
         this.snuggles = 10;
-        this.type = "cat";
-        delete this.discipline;
-        delete this.sleep;
-        delete this.bath;
-        // this.foodDecCat();
-        return "Your dog is now a CAT"
+        this.type = "dog";
+    }   
+
+    feed() {
+        this.food = 10;
     }
-}
 
-foodDecCat() {
-    setInterval(() => {
-        this.food--;
-    }, 7000);
-}
+    playWith() {
+        this.play = 10;
+    }
 
+    bathe() {
+        this.bath = 10;
+    }
+
+    train() {
+        this.discipline = 10;
+    }
+
+    waste() {
+        this.potty = 10;
+    }
+
+    rest() {
+        this.sleep = 10;
+    }
+
+    love() {
+        this.snuggles = 10;
+    }
+
+    foodDecDog() {
+        switch(true) {
+            case (this.type === "dog"):
+                setInterval(() => {
+                    this.food--;       
+                    if(this.food <= 0) {
+                    this.food = 10;
+                    this.play = 10;
+                    this.potty = 10;
+                    this.snuggles = 10;
+                    this.type = "cat";
+                    delete this.discipline;
+                    delete this.sleep;
+                    delete this.bath;
+                    clearInterval(this);
+                    this.foodDecDog();
+                    }
+                }, 5001);
+            case (this.type === "cat"):
+                setInterval(() => {
+                    this.food--;
+                    if(this.food <= 0) {
+                        this.food = 10;
+                        this.bath = 10;
+                        this.type = "fish"
+                        delete this.play;
+                        delete this.snuggles;
+                        delete this.potty;
+                    }
+                }, 7000);
+            // case (this.type = "fish"):
+            //     setInterval(() => {
+            //         this.food--;
+            //     }, 7000);
+            //     if(this.food <= 0) {
+            //         this.food = 10;
+            //         this.bath = 10;
+            //         this.type = "fish"
+            //         delete this.play;
+            //         delete this.snuggles;
+            //         delete this.potty;
+            //     }
+        }           
+    
+    }
 }
